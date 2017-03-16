f143580b06a921506c890bdace70d51f92e2acd6
Antonio Santana <antcesar.santana@gmail.com>
2017-03-16 18:33:17 -0300

    modificate this codigo

--- lista-encadeada.c
+++ lista-encadeada.c
@@ -1,32 +1,70 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-// Declaração da lista
-typedef struct Lista {
-  int valor;
-  struct Lista *prox;
-} NoLista, *Lista;
+#define true 1
+#define false 0
+
+typedef struct Lista{
+	int chave;
+	struct Lista *prox;
+} Lista, *ponteiroLista;
 
 
-// Declaração das funções utilizadas
-void inicializar(Lista *lista);
-_Bool inserir(Lista *lista, int valor);
-_Bool remover(Lista *lista, int valor);
+void inicializar(ponteiroLista *Lista);
+_Bool consultar(ponteiroLista *Lista, int valor);
+_Bool inserir(ponteiroLista *Lista, int valor);
+void imprimir(ponteiroLista *Lista);
 
-/* Função principal */
-int main() {
-  NoLista *lista;
-  inicializar(&lista);
 
-  return 0;
+int main(void) {
+	ponteiroLista lista;
+	int i;
+	inicializar(&lista);
+
+	for(i = 0; i < 11 ; i++) {
+		inserir(&lista,i); //inserindo de 0 a 10 na lista
+	}
+	imprimir(&lista);
+
+	for (i = 0; i < 11; i++) {
+		if (consultar(&lista, i)) {
+			printf("O numero %d pertence a lista\n", i);
+		} else {
+			printf("O numero %d nao pertence a lista\n", i);
+		}
+	}
+	return EXIT_SUCCESS;
 }
 
-// Funções auxiliares
-void inicializar(Lista *lista){
-  *lista = NULL;
+
+void inicializar(ponteiroLista *Lista){
+	*Lista = NULL;
 }
 
+ponteiroLista consultarendereco(ponteiroLista *Lista, int x){
+	ponteiroLista ponteiro;
+	for(ponteiro = *Lista; (ponteiro) && (ponteiro->chave != x); ponteiro = ponteiro->prox);
+	return ponteiro;
+}
 
-_Bool inserir(Lista *lista, int valor) {}
+_Bool consultar(ponteiroLista *Lista, int valor){
+	return (consultarendereco(Lista, valor));
+}
 
-_Bool remover(Lista *lista, int valor){}
+_Bool inserir(ponteiroLista *Lista, int valor){
+	ponteiroLista novo;
+	if(!(novo=(ponteiroLista)malloc(sizeof(Lista))))
+		return false;
+	novo->chave = valor;
+	novo->prox = *Lista;
+	*Lista = novo;
+	return true;
+}
+
+void imprimir(ponteiroLista *Lista){
+	ponteiroLista ponteiro;
+	printf("\n");
+	for(ponteiro = *Lista; (ponteiro) ; p = ponteiro->prox)
+		printf("%d , ", ponteiro->chave);
+	printf("\n");
+}
